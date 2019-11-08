9d9a8d68dd697d97d6612ba143b8fa4c37d2d447
cecil john tantay <cjbtantay@gmail.com>
2019-11-08 13:31:55 -0800

    finish robot_sort

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -1,3 +1,4 @@
+import random
 class SortingRobot:
     def __init__(self, l):
         """
@@ -122,38 +123,51 @@ class SortingRobot:
         """
         self.set_light_on()
         while self.light_is_on():
+            while self.can_move_left():
+                self.move_left()
             self.set_light_off()
             while self.can_move_right(): # [15, 24, 50] item=None | [50, 22, 1] item=None 
-                # print(self._position)
-                # print('before', self._item)
-                self.swap_item() # [(None), 24, 50] item=15 | [50, 22, 1] item=None 
-                # print('after', self._item)
-                # self.move_right() # [None, (24), 50] item=15  | [50, 22, 1] item=None 
-                
-                if self.compare_item() == -1:
-                    print(self._list)
-                    print('positon', self._position)
-                    self.move_left() # [(None), 24, 50] item=15  | [50, 22, 1] item=None 
-                    # print('before compare', self._item)
-                    self.swap_item() # [(15), 24, 50] item=None  | [50, 22, 1] item=None 
-                    # print('after compare', self._item)
-                    self.move_right() # [15, (24), 50] item=None  | [50, 22, 1] item=None 
-
-                if self.compare_item() == 1:
-                    # print('before compare', self._list)
-
-                    self.swap_item() 
-                    self.move_left()
+                if self.compare_item() == None:
                     self.swap_item()
-                    # print('before compare', self._list)
                     self.move_right()
-                    self.set_light_on()
-
-                    # print(self.light_is_on())
-
-                # print(self._item)
+                    if self.compare_item() == 1:
+                        self.swap_item()
+                        self.move_left()
+                        self.swap_item()
+                        self.set_light_on()
+                    if self.compare_item() == -1:
+                        self.move_left()
+                        self.swap_item()
+                    if self.compare_item() == 0:
+                        self.move_left()
+                        self.swap_item()
                 self.move_right()
-                print(self.can_move_right())
+
+
+                # if self.compare_item() == -1:
+                #     print(self._list)
+                #     print('positon', self._position)
+                #     self.move_left() # [(None), 24, 50] item=15  | [50, 22, 1] item=None 
+                #     # print('before compare', self._item)
+                #     self.swap_item() # [(15), 24, 50] item=None  | [50, 22, 1] item=None 
+                #     # print('after compare', self._item)
+                #     self.move_right() # [15, (24), 50] item=None  | [50, 22, 1] item=None 
+
+                # if self.compare_item() == 1:
+                #     # print('before compare', self._list)
+
+                #     self.swap_item() 
+                #     self.move_left()
+                #     self.swap_item()
+                #     # print('before compare', self._list)
+                #     self.move_right()
+                #     self.set_light_on()
+
+                #     # print(self.light_is_on())
+
+                # # print(self._item)
+                # self.move_right()
+                # print(self.can_move_right())
             
 
       
@@ -165,11 +179,14 @@ if __name__ == "__main__":
     # with `python robot_sort.py`
 
     # l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
+    l = [1, -38, -95, 4, 23, -73, -65, -36, 85, 2, 58, -26, -55, 96, 55, -76, 64, 45, 69, 36, 69, 47, 29, -47, 13, 89, -57, -88, -87, 54, 60, 56, -98, -78, 59, 93, -41, -74, 73, -35, -23, -79, -35, 46, -18, -18, 37, -64, 14, -57, -2, 15, -85, 45, -73, -2, 79, -87, -100, 21, -51, 22, 26, -59, 81, 59, -24, 24, -81, 43, 61, 52, 38, -88, -95, 87, -57, -37, -65, -47, -3, 21, -77, 98, 25, 1, -36, 39, 78, 47, -35, -40, -69, -81, 11, -47, 21, 25, -53, -31]
+    # print([random.randint(0, 100) for i in range(0, 100)])
 
-    l = [50, 22, 1, 4, 5, 43]
+    # l = [50, 22, 14]
     # l= [15, 24, 50]
 
     robot = SortingRobot(l)
 
     robot.sort()
     print(robot._list)
+    print(robot._item)
